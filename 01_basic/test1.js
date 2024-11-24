b5bc4c21aa68634608c87e1e4d9d23032a65e30f
Arin raja <133584440+Arinraja@users.noreply.github.com>
2024-11-24 14:45:02 +0000

    commit

---
+++ 01_basic/test1.js
@@ -0,0 +1,23 @@
+console.log("Arin Raja")
+
+const accountId = 14413213
+let accountEmail = "arin@google.com"
+var accountPassword = "122323"
+let accountcit = "noida"
+
+
+// accountId = 2  //   NOT ALLOWED
+/*
+
+PREFER NOT TO USE THE var
+BECAUSE OF THE ISSUE OF THE BLOCK SCOPE AND FUNCTIONAL SCOPE
+
+*/
+
+accountEmail ="rinkoo@google.com"
+accountPassword= "902389"
+accountcit = "agra"
+ console.log(accountId);
+ console.log([accountcit, accountId, accountEmail, accountPassword]);
+
+ console.table([accountcit, accountId, accountEmail, accountPassword]);
