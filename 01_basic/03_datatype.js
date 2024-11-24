87b636be10169de95ddfe3c58a0e13bd22f0185f
Arin raja <133584440+Arinraja@users.noreply.github.com>
2024-11-24 15:04:06 +0000

    commit

---
+++ 01_basic/03_datatype.js
@@ -0,0 +1,18 @@
+ "use strict ";  // treat all js code as newer version
+
+//  alert( 3+ 3 )  // WE ARE USING THE NODE JS NOT THE BROWSER 
+
+// console.log(3+ 3 ); console.log("arin"); // Not the good practice to write in the same line 
+
+  // number => 2 to power 53
+  // bigint 
+  // string => ""
+  // boolean => true/false
+  // null => standalone value
+// undefined =>
+    // symbol => unique
+
+
+// object 
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
